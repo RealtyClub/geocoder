710322867c99de21de35f61a3280138c37986484
Roger Smith <rogsmith@gmail.com>
2023-02-09 23:47:42 -0500

    convert full state to abbr state

--- lib/geocoder/results/amazon_location_service.rb
+++ lib/geocoder/results/amazon_location_service.rb
@@ -27,11 +27,15 @@ module Geocoder::Result
     end
 
     def state
-      @place.region
+      if @place.region
+        states_lookup.key(@place.region)&.to_s
+      end
     end
 
     def state_code
-      @place.region
+      if @place.region
+        states_lookup.key(@place.region)&.to_s
+      end
     end
 
     def province
@@ -64,6 +68,65 @@ module Geocoder::Result
 
     def formatted_address
       address
+    end
+
+    def states_lookup
+      {  AK: "Alaska",
+        AL: "Alabama",
+        AR: "Arkansas",
+        AS: "American Samoa",
+        AZ: "Arizona",
+        CA: "California",
+        CO: "Colorado",
+        CT: "Connecticut",
+        DC: "District of Columbia",
+        DE: "Delaware",
+        FL: "Florida",
+        GA: "Georgia",
+        GU: "Guam",
+        HI: "Hawaii",
+        IA: "Iowa",
+        ID: "Idaho",
+        IL: "Illinois",
+        IN: "Indiana",
+        KS: "Kansas",
+        KY: "Kentucky",
+        LA: "Louisiana",
+        MA: "Massachusetts",
+        MD: "Maryland",
+        ME: "Maine",
+        MI: "Michigan",
+        MN: "Minnesota",
+        MO: "Missouri",
+        MS: "Mississippi",
+        MT: "Montana",
+        NC: "North Carolina",
+        ND: "North Dakota",
+        NE: "Nebraska",
+        NH: "New Hampshire",
+        NJ: "New Jersey",
+        NM: "New Mexico",
+        NV: "Nevada",
+        NY: "New York",
+        OH: "Ohio",
+        OK: "Oklahoma",
+        OR: "Oregon",
+        PA: "Pennsylvania",
+        PR: "Puerto Rico",
+        RI: "Rhode Island",
+        SC: "South Carolina",
+        SD: "South Dakota",
+        TN: "Tennessee",
+        TX: "Texas",
+        UT: "Utah",
+        VA: "Virginia",
+        VI: "Virgin Islands",
+        VT: "Vermont",
+        WA: "Washington",
+        WI: "Wisconsin",
+        WV: "West Virginia",
+        WY: "Wyoming"
+      }
     end
   end
 end
